33e5b2d75afc8178bdd35e6dacdffab24861f92e
Muhammad Ahmed <itsahmed.dev@gmail.com>
2021-12-22 20:52:55 +0500

    add comment examples

---
+++ comments.dart
@@ -0,0 +1,19 @@
+// Sinle line comments:
+//[main] is a funtion in program below and this is a line comment.
+// Notice there isn't any link between funtion [main] to
+// the program below
+
+/*
+  Multiline comments:
+  These are multi-line comments. There isn't any connection
+  between funtion [main] and program below
+*/
+
+/// Document Comments:
+/// [main] is a funtion and this is Document comment
+/// Notice that main funtion in brackets above corresponds to main funtion below
+/// That's what makes document comments different.
+
+void main() {
+  print('بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ');
+}
