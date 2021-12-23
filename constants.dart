5ab0ba7f3ad2ff813032082512223cce9b8015bb
Muhammad Ahmed <itsahmed.dev@gmail.com>
2021-12-23 16:27:11 +0500

    Program for final and const keywords.

---
+++ constants.dart
@@ -0,0 +1,21 @@
+void main() {
+  // final
+  final cityName = 'Mirpur';
+  //	name = 'Ahmed';     // Throws an error
+
+  final String countryName = 'Pakistan';
+
+  // const
+  const pi = 3.14;
+  const double gravity = 9.8;
+
+  print(cityName);
+  print(countryName);
+  print(pi);
+  print(gravity);
+}
+
+class Circle {
+  final color = 'Blue';
+  static const pi = 3.14;
+}
