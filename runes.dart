42d4ed60b3b064672b6a9d58d0e6668a7ab4f4e6
Muhammad Ahmed <itsahmed.dev@gmail.com>
2021-12-24 14:52:51 +0500

    Example program for Dart Data Type RUNES

---
+++ runes.dart
@@ -0,0 +1,9 @@
+void main() {
+  // The special heart character ♥ is equivalent to Unicode code \u2665
+  // '\u' means Unicode and the numbers are hexadecimal integers
+  var heart_symbol = '\u2665';
+  // Hex value is greater than 4 digits so it's placed in {curly bracket}
+  var laugh_emoji = '\u{1f600}';
+  print(heart_symbol);
+  print(laugh_emoji);
+}
