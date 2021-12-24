44ba381160bad4abfd37dc076cfe84c3ac0492a4
Muhammad Ahmed <itsahmed.dev@gmail.com>
2021-12-24 18:33:59 +0500

    Relational Operators

---
+++ relationalOperators.dart
@@ -0,0 +1,47 @@
+void main() {
+  // variable declaration with int data type
+  int a = 5;
+  int b = 2;
+
+  // Equal to
+  if (a == b) {
+    print('$a is equal to $b');
+  } else {
+    print('$a is not equal to $b');
+  }
+
+  // Not equal to
+  if (b != a) {
+    print('$b is not equal to $a');
+  } else {
+    print('$b is equal to $a');
+  }
+
+  // Greater than
+  if (a > b) {
+    print('$a is greater than $b');
+  } else {
+    print('$a is not greater than $b');
+  }
+
+  // Less than
+  if (a < b) {
+    print('$a is less than $b');
+  } else {
+    print('$a is not less than $b');
+  }
+
+  // Less than or equal to
+  if (a <= b) {
+    print('$a is less than or equal to $b');
+  } else {
+    print('$a is not less than or equal to $b');
+  }
+
+  //Greater than or equal to
+  if (a >= b) {
+    print('$a is greater than or equal to $b');
+  } else {
+    print('$a is not greater than or equal to $b');
+  }
+}
