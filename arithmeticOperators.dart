2e4eeb9329ec288324c7e6c2bdb4b39799b9d2dd
Muhammad Ahmed <itsahmed.dev@gmail.com>
2021-12-24 17:55:24 +0500

    Arithmetic Operators

---
+++ arithmeticOperators.dart
@@ -0,0 +1,16 @@
+void main() {
+  // variable declaration with int data type
+  int a = 5;
+  int b = 2;
+
+  // Addition
+  print(a + b);
+  // Subtraction
+  print(a - b);
+  // Multiplication
+  print(a * b);
+  // Division
+  print(a / b);
+  // Modulus
+  print(a % b);
+}
