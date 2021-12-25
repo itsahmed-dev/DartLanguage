0847c60e73277235af0573b15efebd7c0e69da23
Muhammad Ahmed <itsahmed.dev@gmail.com>
2021-12-25 15:21:13 +0500

    Example program for Funtions in Dart (GeekForGeek)

---
+++ Functions/Funtion2.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Calling the funtion
+  var result = add(5, 10);
+  // printing the output
+  print(result);
+}
+
+int add(int a, int b) {
+  // Main job of the funtion
+  var calculation = a + b;
+  // Returning the output
+  return calculation;
+}
