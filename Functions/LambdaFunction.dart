2f401996da5dd72fb043f3152f15c92560a02312
Muhammad Ahmed <itsahmed.dev@gmail.com>
2021-12-25 16:23:45 +0500

    Example program for a lambda Funtion in Dart (GeekForGeek)

---
+++ Functions/LambdaFunction.dart
@@ -0,0 +1,8 @@
+// Declaring a Lambda funtion
+prompt() => print("It's a Lambda Funtion in Dart");
+void main() {
+  // Printing output title
+  print('Prompt: ');
+  // Calling the funtion
+  prompt();
+}
