7a84fce77e228073dd672ae84f404fbfe3de7be8
Muhammad Ahmed <itsahmed.dev@gmail.com>
2021-12-27 10:20:43 +0500

    Example Program for Map data type

---
+++ DataTypes/Maps.dart
@@ -0,0 +1,14 @@
+void main() {
+  // variable decleration with map data type
+  var weekDays = {
+    'day1': 'Monday',
+    'day2': 'Tuesday',
+    'day3': 'Wednesday',
+    'day4': 'Thursday',
+    'day5': 'Friday',
+    'day6': 'Saturday',
+    'day7': 'Sunday'
+  };
+  // accessing value inside Map using [] and printing it
+  print(weekDays['day5']);
+}
