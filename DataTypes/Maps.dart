b8ecbc92383c5a1f782ad93e0960e5eb0861245c
Muhammad Ahmed <itsahmed.dev@gmail.com>
2021-12-28 14:59:16 +0500

    Added another example program

--- DataTypes/Maps.dart
+++ DataTypes/Maps.dart
@@ -9,6 +9,21 @@ void main() {
     'day6': 'Saturday',
     'day7': 'Sunday'
   };
+  print('First Example:');
   // accessing value inside Map using [] and printing it
   print(weekDays['day5']);
+  // accesing tire map
+  print(weekDays);
+  // Another Example of Maps
+  var weekDays2 = new Map();
+  weekDays2['dOne'] = 'Monday';
+  weekDays2['dTwo'] = 'Tuesday';
+  weekDays2['dThree'] = 'Wednesday';
+  weekDays2['dFour'] = 'Thursday';
+  weekDays2['dFive'] = 'Friday';
+  weekDays2['dSix'] = 'Saturday';
+  weekDays2['dSeven'] = 'Sunday';
+  print('Second Example:');
+  print(weekDays2['dFive']);
+  print(weekDays2);
 }
