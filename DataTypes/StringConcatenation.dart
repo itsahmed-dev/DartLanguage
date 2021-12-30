8f6bff0d5b7349c1484c9920ddbad7bf97b2e978
Muhammad Ahmed <itsahmed.dev@gmail.com>
2021-12-30 21:18:13 +0500

    Example Program for String Concatenation

---
+++ DataTypes/StringConcatenation.dart
@@ -0,0 +1,11 @@
+void main() {
+  String firstName = 'Muhammad ';
+  String lastName = 'Ahmed';
+  // String Concatenation
+  // Not a convention of Dart
+  print(firstName + lastName);
+  // String Concatenation Example 2
+  String a = 'Dart ';
+  String b = 'Programming Language';
+  print(a += b);
+}
