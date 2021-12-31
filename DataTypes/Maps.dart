b0dfa492116ed92eb19957526b5b1a4abdbf9e2f
Muhammad Ahmed <itsahmed.dev@gmail.com>
2021-12-31 15:27:11 +0500

    Added Map Properties and Methods

--- DataTypes/Maps.dart
+++ DataTypes/Maps.dart
@@ -1,5 +1,7 @@
 void main() {
   // variable decleration with map data type
+
+  // Dart Map Literal Declaration
   var weekDays = {
     'day1': 'Monday',
     'day2': 'Tuesday',
@@ -9,12 +11,27 @@ void main() {
     'day6': 'Saturday',
     'day7': 'Sunday'
   };
+
+  // Adding value at runtime
+  weekDays['day8'] = 'Myday';
   print('First Example:');
+
   // accessing value inside Map using [] and printing it
   print(weekDays['day5']);
-  // accesing tire map
+
+  // accesing entire map
   print(weekDays);
+
+  // Map Properties
+  print('They keys are: ${weekDays.keys}');
+  print('The values are: ${weekDays.values}');
+  print('The length of the Map is: ${weekDays.length}');
+  print('The Map is empty: ${weekDays.isEmpty}');
+  print('The Map is not empty: ${weekDays.isNotEmpty}');
+
   // Another Example of Maps
+
+  // Dart Map Constructor Declaration
   var weekDays2 = new Map();
   weekDays2['dOne'] = 'Monday';
   weekDays2['dTwo'] = 'Tuesday';
@@ -26,4 +43,16 @@ void main() {
   print('Second Example:');
   print(weekDays2['dFive']);
   print(weekDays2);
+
+  // addAll method
+  weekDays2.addAll({'dEight': 'Myday'});
+  print('After addAll() method ${weekDays2}');
+
+  // remove() method
+  weekDays2.remove('dEight');
+  print('After remove() method ${weekDays2}');
+
+  // clear() method
+  weekDays2.clear();
+  print('Map after removing all key-values :${weekDays2}');
 }
