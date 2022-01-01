bdfb984aa67efd49f7f7ad10b0631870e563c326
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-01-01 10:30:06 +0500

    Added another example of Enumeration

--- DataTypes/Enumeration.dart
+++ DataTypes/Enumeration.dart
@@ -1,7 +1,26 @@
 enum workDays { Sat, Sun, Mon, Tue, Wed, Thu }
+enum monthsYear {
+  January,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December
+}
 void main() {
   print('Dart Enumeration');
+  print('Working Days in a Week');
   print(workDays.values);
+
   // Not sure whats happing here
   workDays.values.forEach((v) => print('value: $v, index: ${v.index}'));
+
+  print('Months in a Year');
+  print(monthsYear.values);
 }
