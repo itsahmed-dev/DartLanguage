56daa99d7a5129849aab2d622a7e8b445b0bfbf3
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-01-06 10:20:32 +0500

    Example Program for Recursive Function in Dart

---
+++ Functions/RecursiveFunction0.dart
@@ -0,0 +1,11 @@
+sayHi(int i) {
+  if (i >= 1) {
+    print('Hi $i');
+    sayHi(i - 1); // Function Calling itself hence recursive function
+  }
+}
+
+void main() {
+  print('Recursive Funtion: ');
+  sayHi(5);
+}
