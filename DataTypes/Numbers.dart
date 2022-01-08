e494f1898d8325e46d1f073e7e754971f71b683c
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-01-08 20:41:45 +0500

    Added example for Interpolation for int and double

--- DataTypes/Numbers.dart
+++ DataTypes/Numbers.dart
@@ -1,6 +1,20 @@
 void main() {
+  int number = 123;
+  print(number);
+  print(''); // Line-Break
+
+  // hexValue is also int data type
   int hexValue = 0x0312;
   print(hexValue);
+  print(''); // Line-Break
+
+  // exponents are also double data type
   double exponent = 1.42e5;
   print(exponent);
+  print(''); // Line-Break
+
+  // Interpolation also works on ints and doubles as well like so
+  int a = 10;
+  int b = 20;
+  print('The sum of $a and $b is ${a + b}');
 }
