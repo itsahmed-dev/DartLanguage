32329d5477e14542a8b8009f60a1208c10dc9d36
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-01-09 17:50:41 +0500

    Added more examples

--- ControlFlow/IterationStatements.dart
+++ ControlFlow/IterationStatements.dart
@@ -4,6 +4,7 @@ void main() {
   for (int counter = 1; counter <= 3; counter++) {
     print(counter);
   }
+  print('');
 
   // for loop example: 2
   print('Nested For Loop in Dart');
@@ -12,6 +13,7 @@ void main() {
       print('$i $j');
     }
   }
+  print('');
 
   // for loop example: 3
   print('Even numbers between 0 and 10');
@@ -20,6 +22,7 @@ void main() {
       print(i);
     }
   }
+  print('');
 
   // for loop example: 4
   print('Odd numbers between 0 and 10');
@@ -28,6 +31,15 @@ void main() {
       print(i);
     }
   }
+  print('');
+
+  // for loop example: 5
+  print('Math Table using for loop');
+  int tableOf = 5;
+  for (var number = 1; number <= 10; number++) {
+    print('$tableOf * $number = ${tableOf * number}');
+  }
+  print('');
 
   // for-in loop example: 1
   print('For-in Loop in Dart Example: 1');
@@ -36,6 +48,7 @@ void main() {
   for (var ctr in counter) {
     print(ctr);
   }
+  print('');
 
   // for-in loop example: 2
   print('For-in Loop in Dart Example: 2');
@@ -51,8 +64,10 @@ void main() {
   for (String planet in planets) {
     print(planet);
   }
+  print('');
 
   // while loop
+  // Example 1
   print('While Loop in Dart');
   var anotherCounter = 1;
   var maxCount = 3;
@@ -60,8 +75,30 @@ void main() {
     print(anotherCounter);
     anotherCounter++;
   }
+  print('');
+
+  // Example 2
+  print('Math Table using while loop');
+  int x = 5;
+  int y = 1;
+  while (y <= 10) {
+    print('$x * $y = ${x * y}');
+    y++;
+  }
+  print('');
 
   // do-while loop
+  // Example 1
+  print('Math Table using do-while loop');
+  int tableNum = 5;
+  int z = 1;
+  do {
+    print('$tableNum * $z = ${tableNum * z}');
+    z++;
+  } while (z <= 10);
+  print('');
+
+  // Example 2
   print('Do-While Loop in Dart');
   var lastCounter = 1;
   var maxCounter = 3;
