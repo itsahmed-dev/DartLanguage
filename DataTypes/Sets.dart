e96e091fbaaabf78ad96ea63d895e3fff068d7d6
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-01-10 11:06:21 +0500

    Added more Examples

--- DataTypes/Sets.dart
+++ DataTypes/Sets.dart
@@ -1,9 +1,36 @@
 void main() {
+  // String Set Example
   var name = <String>{
     "Ali",
     "Ahmed",
     "Adnan",
   };
   print('Set Initialization in Dart');
-  print(name);
+  print('Set of names $name');
+
+  // int Set Example
+  var age = {
+    18,
+    20,
+    30,
+  };
+  print('Set of Ages $age');
+  print(''); // Line-Break
+
+  // Spread Operator
+  print('Combing Sets using ...Spread Operaotr');
+  var abc = {'a', 'b', 'c'};
+  var num = {1, 2, 3, ...abc};
+  print(num);
+  print(''); // Line-Break
+
+  // for loop in Sets
+  print('Using for loop inside the set');
+  var abcd = {
+    'a',
+    'b',
+    'c',
+    for (var i = 1; i <= 3; i++) 'd $i',
+  };
+  print(abcd);
 }
