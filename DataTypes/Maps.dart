9b88dfde049007046d8a4fbd1386d7940165c111
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-01-10 14:42:08 +0500

    Added more Examples

--- DataTypes/Maps.dart
+++ DataTypes/Maps.dart
@@ -16,18 +16,21 @@ void main() {
   weekDays['day8'] = 'Myday';
   print('First Example:');
 
-  // accessing value inside Map using [] and printing it
+  // Accessing value inside Map using [] and printing it
   print(weekDays['day5']);
 
-  // accesing entire map
+  // Accesing entire map
   print(weekDays);
+  print(''); // Line-Break
 
   // Map Properties
+  print('Properties of Map Data Type');
   print('They keys are: ${weekDays.keys}');
   print('The values are: ${weekDays.values}');
   print('The length of the Map is: ${weekDays.length}');
   print('The Map is empty: ${weekDays.isEmpty}');
   print('The Map is not empty: ${weekDays.isNotEmpty}');
+  print(''); // Line-Break
 
   // Another Example of Maps
 
@@ -43,16 +46,28 @@ void main() {
   print('Second Example:');
   print(weekDays2['dFive']);
   print(weekDays2);
+  print(''); // Line-Break
 
-  // addAll method
+  // addAll() method
   weekDays2.addAll({'dEight': 'Myday'});
   print('After addAll() method ${weekDays2}');
+  print(''); // Line-Break
 
   // remove() method
   weekDays2.remove('dEight');
   print('After remove() method ${weekDays2}');
+  print(''); // Line-Break
 
   // clear() method
   weekDays2.clear();
   print('Map after removing all key-values :${weekDays2}');
+  print(''); // Line-Break
+
+  // Maps Example
+  var users = {
+    1: 'Ahmed',
+    2: 'Ahmed',
+    3: 'Ahmed',
+  };
+  print('User at ID 1: ${users[1]}');
 }
