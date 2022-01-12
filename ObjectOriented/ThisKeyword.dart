147950ff35c401dfd7f41e7a376db843afe7bf29
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-01-12 11:40:25 +0500

    Example Program for This Keyword in Dart

---
+++ ObjectOriented/ThisKeyword.dart
@@ -0,0 +1,12 @@
+void main() {
+  Employee emp = new Employee('EMP001');
+}
+
+class Employee {
+  String? emp_code;
+  Employee(String emp_code) {
+    this.emp_code = emp_code;
+    print('Dart this Keyword Example');
+    print('The Employee Code: ${emp_code}');
+  }
+}
