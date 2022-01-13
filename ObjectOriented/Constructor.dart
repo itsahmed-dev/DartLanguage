43928efe478b02329a7772c110b9ed83b3ef0949
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-01-13 20:50:04 +0500

    made minor changes

--- ObjectOriented/Constructor.dart
+++ ObjectOriented/Constructor.dart
@@ -1,24 +1,21 @@
 // ignore_for_file: unused_local_variable
 
-void main() {
-  print('Dart Constructor Example');
-  Employee emp = new Employee('EP001');
-  print(''); // Line-Break
+// Students Class Example
+class Students {
+  int? id;
+  String? name;
 
-  print('Dart Default Constructor');
-  DefaultCon dcom = new DefaultCon();
-  print(''); // Line-Break
+  Students(int id, String name) {
+    print('This is the body of defualt constructor in Student class');
+    print('The ID is: $id and the Student name is: $name');
+  }
 
-  print('Dart Parameterized Constructor');
-  ParaCon code = new ParaCon('PC001');
-  print(''); // Line-Break
-
-  print('Dart Named Constructor');
-  NamedCon ncon = new NamedCon();
-  NamedCon ncon2 = new NamedCon.namedConst('NC001');
+  studying() {
+    return studying;
+  }
 }
 
-// Class
+// Employee Class Example
 class Employee {
   // Constructor
   Employee(String empCode) {
@@ -26,30 +23,47 @@ class Employee {
   }
 }
 
-// Class
 class DefaultCon {
   // Default Constructor or no Argument Constructor
   DefaultCon() {
-    print('Default Constructor Body of DefaultCon Class');
+    print('Default Constructor Body in the Class');
   }
 }
 
-// Class
 class ParaCon {
   // Parameterized Constructor
   ParaCon(String Code) {
-    print('Parameterized Constructor Body of ParaCon Class');
+    print('Parameterized Constructor Body in the Class');
   }
 }
 
-// Class
 class NamedCon {
   // Named Constructor
   NamedCon() {
     print('Default Constructor Body of NamedCon Class');
   }
   NamedCon.namedConst(String Code) {
-    print('Parameterized Constructor Body of NamedCon Class');
+    print('Parameterized Constructor Body in the Class');
     print(Code);
   }
+}
+
+void main() {
+  var student1 = Students(786, 'Ahmed');
+
+  print('Dart Constructor Example');
+  var emp = Employee('EP001');
+  print(''); // Line-Break
+
+  print('Dart Default Constructor');
+  var dcom = DefaultCon();
+  print(''); // Line-Break
+
+  print('Dart Parameterized Constructor');
+  var code = ParaCon('PC001');
+  print(''); // Line-Break
+
+  print('Dart Named Constructor');
+  var ncon = NamedCon();
+  var ncon2 = NamedCon.namedConst('NC001');
 }
