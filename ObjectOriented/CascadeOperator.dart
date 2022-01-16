9a58b2d5c0f9ceb8c85228634aa9be90b96bb10a
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-01-16 18:46:02 +0500

    Exampel Program for Cascade Operator in Dart

---
+++ ObjectOriented/CascadeOperator.dart
@@ -0,0 +1,16 @@
+class Student {
+  void test1() {
+    print('The Cascade Operator 1');
+  }
+
+  void test2() {
+    print('The Cascade Operator 2');
+  }
+}
+
+void main() {
+  new Student()
+    // Using Cascade Operator (..)
+    ..test1()
+    ..test2();
+}
