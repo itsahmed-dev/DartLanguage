14399db69147554392addabee7731079646f8681
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-01-18 11:26:01 +0500

    Example Program for Method Overriding in Dart

---
+++ ObjectOriented/MethodOverriding.dart
@@ -0,0 +1,42 @@
+class Human {
+  void run() {
+    print('Human is running');
+  }
+}
+
+class Man extends Human {
+  void run() {
+    print('Man is running');
+  }
+}
+
+class Human2 {
+  void run() {
+    print('Human is running');
+  }
+}
+
+class Man2 extends Human2 {
+  void run() {
+    super.run();
+    print('Man is running');
+  }
+}
+
+void main() {
+  // Method Overriding
+  print('Method Overriding');
+  var m = Man();
+  m.run();
+  print(''); // Line-Break
+
+  // Method Overriding with super
+  print('Method Overriding with super keyword');
+  var m2 = Man2();
+  m2.run();
+  print(''); // Line-Break
+
+  // Method Overriding with Cascade Notation (..)
+  print('Method Overriding with Cascade Notation');
+  Man()..run();
+}
