0a94cfe03a52918aef6696e1e1b491180fd04f48
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-01-22 10:55:08 +0500

    Example Program for a Single Interface in Dart

---
+++ ObjectOriented/Interfaces.dart
@@ -0,0 +1,21 @@
+// Dart Interfaces
+
+// Declaring an Interface
+class Student {
+  void studentInfo() {
+    print('I am a Student.');
+  }
+}
+
+// Implementing an Interface
+class CSStudent implements Student {
+  void studentInfo() {
+    print('I am a Computer Science Student.');
+  }
+}
+
+void main() {
+  print('Dart Interfaces Example: ');
+  var student = CSStudent();
+  student.studentInfo();
+}
