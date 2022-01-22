41224d164d4803640902e9c181de70a8e50e120c
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-01-22 12:58:37 +0500

    Added another Example

--- ObjectOriented/MethodOverriding.dart
+++ ObjectOriented/MethodOverriding.dart
@@ -1,6 +1,6 @@
 // Method Overriding in Dart
 
-//Example 1
+// Example 1
 class Human {
   void run() {
     print('Human is running');
@@ -13,7 +13,7 @@ class Man extends Human {
   }
 }
 
-//Example 2
+// Example 2
 class Human2 {
   void run() {
     print('Human is running');
@@ -27,13 +27,40 @@ class Man2 extends Human2 {
   }
 }
 
+// Example 3
+class Animals {
+  int? age;
+
+  void eat() {
+    // Method to be Overridden
+    print('Animal is Eating...');
+  }
+}
+
+class Dog extends Animals {
+  String? breed;
+
+  void eat() {
+    // Method Overrriding
+    print('Dog is Eating...');
+    //  Calling Method Overridden
+    super.eat();
+    // Next Statement
+    print('Both are eating...');
+  }
+}
+
 void main() {
   // Method Overriding
+
+  // Example 1:
   print('Method Overriding');
   var m = Man();
   m.run();
 
   print(''); // Line-Break
+
+  // Example 2:
 
   // Method Overriding with super
   print('Method Overriding with super keyword');
@@ -45,4 +72,13 @@ void main() {
   // Method Overriding with Cascade Notation (..)
   print('Method Overriding with Cascade Notation');
   Man()..run();
+
+  // Example 3:
+
+  print('DOG\n');
+  var dog = Dog();
+  dog.breed = 'Poodle';
+  dog.age = 2;
+  print('The Dog Breed is ${dog.breed} and Age is ${dog.age}');
+  dog.eat();
 }
