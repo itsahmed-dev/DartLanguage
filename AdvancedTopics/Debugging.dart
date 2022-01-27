445145cef3182539ea582530e5d4bfb51fe581ec
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-01-27 09:43:02 +0500

    Example Program for Debugging in Dart

---
+++ AdvancedTopics/Debugging.dart
@@ -0,0 +1,12 @@
+// Debugging in Dart
+
+void main() {
+  int a = 10, b = 20, c = 5;
+  c = c * c * c;
+
+  print("$a + $b = ${a + b}");
+  print("$a%$b = ${a % b}"); // Add a break point here
+  print("$a*$b = ${a * b}");
+  print("$a/$b = ${a / b}");
+  print(c);
+}
