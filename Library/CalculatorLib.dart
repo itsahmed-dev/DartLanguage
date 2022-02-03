6ab1caaf2fecbdc5a6ee737c0f9f8fdc671aa0f1
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-02-03 10:24:07 +0500

    Calculator Libray file for Library.dart

---
+++ Library/CalculatorLib.dart
@@ -0,0 +1,22 @@
+library calculator;
+
+//library content
+int add(int num1, int num2) {
+  print("inside add method of calculator Library");
+  return num1 + num2;
+}
+
+int subtract(int num1, int num2) {
+  print("inside subtract method of calculator Library");
+  return num1 - num2;
+}
+
+int multiply(int num1, int num2) {
+  print("inside multiply method of calculator Library");
+  return num1 * num2;
+}
+
+double divide(int num1, int num2) {
+  print("inside divide method of calculator Library");
+  return num1 / num2;
+}
