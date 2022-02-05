a5376419cee483f82f85848fb429fe2603907518
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-02-05 08:18:43 +0500

    Isolates in Dart

---
+++ AdvancedTopics/Isolates.dart
@@ -0,0 +1,15 @@
+// Isolates in Dart
+
+import 'dart:isolate';
+
+void sayHi(msg) {
+  print('Execution from sayHi($msg)');
+}
+
+void main() {
+  Isolate.spawn(sayHi, 'Hello!');
+  Isolate.spawn(sayHi, 'Whats Up!');
+  Isolate.spawn(sayHi, 'Welcome!');
+
+  print('Execution from main()');
+}
