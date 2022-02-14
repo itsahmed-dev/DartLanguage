60575338802e8e21fa4f92f7fffdc1fcbc3f357a
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-02-14 10:14:08 +0500

    File Renamed

---
+++ MyGitHubGist.dart
@@ -0,0 +1,10 @@
+// Dart Programming Language
+void main() {
+  // intro
+
+  String firstName = 'Muhammad';
+  String lastName = 'Ahmed';
+
+  print('My name is $firstName $lastName');
+  print('You can call me $lastName');
+}
