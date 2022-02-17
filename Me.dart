941fb651aff2e368df16b3a2f37e68d53d5b50f5
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-02-17 11:13:26 +0500

    First commit in Exercises branch

--- Me.dart
+++
@@ -1,9 +0,0 @@
-// Dart Programming Language
-void main() {
-  // intro
-  String firstName = 'Muhammad';
-  String lastName = 'Ahmed';
-
-  print('My name is $firstName $lastName');
-  print('You can call me $lastName');
-}
