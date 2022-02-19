7fdc31390e7657a6b79c3140e4e3381ba01f56ff
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-02-19 10:05:55 +0500

    Generics in Dart by Andrea

---
+++ Collection/Generics.dart
@@ -0,0 +1,13 @@
+// Dart - Collections: Generics
+
+void main() {
+  // Type-safe List via Generics
+  var aList = <int>[];
+  aList.add(0);
+  aList.addAll([1, 2, 3, 4, 5]);
+  print(aList);
+
+  // Type-safe Map via Generics
+  var person = <String, dynamic>{'name': 'Ahmed', 'age': 30, 'gpa': 3.5};
+  print(person['name']);
+}
