8aa636ba94b97d77f9836701e96696bbb8bba591
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-02-21 09:15:01 +0500

    Added more examples from educative

--- Operators/ConditionalOperators.dart
+++ Operators/ConditionalOperators.dart
@@ -1,5 +1,7 @@
 // Dart - Conditional Operators
 
+// Ternary Operator
+
 void main() {
   // Variable declaration
   var exp1 = null;
@@ -8,15 +10,30 @@ void main() {
 
   // Scenario 1:
   // If exp1 is not-null return its value, else return the exp2's value
+  print('Display non-Null value');
   print(exp1 ?? exp2);
+
+  print(''); //  Line-Break
 
   // Scenario 2:
   // First option after ? is True and second one is false
-  var output = exp3 < 42 ? "YES" : "NO";
+  print('Evaluate expression greater or less');
+  var output = exp3 < 42 ? "Less" : "Greater";
   print(output);
+
+  print(''); //  Line-Break
 
   // Scenario 3:
   // If both expressions are greater than 0 then they're not null
-  var scenario3 = exp3 > 0 && exp2 > 0 ? "Exp Not Null" : "Exp Null";
+  print('Null expression checking');
+  var scenario3 = exp3 > 0 && exp2 > 0 ? "Not Null" : "Null";
   print(scenario3);
+
+  print(''); //  Line-Break
+
+  // Scenario 4:
+  print('Even or Add');
+  int num = 5;
+  var eOo = num % 2 == 0 ? '$num is Even' : '$num is odd';
+  print(eOo);
 }
