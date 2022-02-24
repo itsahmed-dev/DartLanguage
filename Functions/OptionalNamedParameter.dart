5d008eb46a3709000d046e7d310f0905920f7453
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-02-24 09:08:52 +0500

    Added more examples

--- Functions/OptionalNamedParameter.dart
+++ Functions/OptionalNamedParameter.dart
@@ -1,12 +1,21 @@
-void main() {
-  print('Optional Named Parameter');
-  testParameter(1);
-  testParameter(1, n1: 2);
-  testParameter(1, n1: 2, n2: 3);
-}
+// Dart - Functions: Parameters
 
+// Optional Named Parameters
+
+// Function Declaration
 testParameter(p1, {n1, n2}) {
   print('Parameter 1: $p1');
   print('Optional Named Parameter 1: $n1');
   print('Optional Named Parameter 2: $n2');
+  print(''); //  Line-Break
+}
+
+void main() {
+  print('Optional Named Parameter');
+
+  print(''); //  Line-Break
+
+  testParameter(1);
+  testParameter(1, n1: 2);
+  testParameter(1, n1: 2, n2: 3);
 }
--- Functions/OptionalPositionalParameters.dart
+++ Functions/OptionalPositionalParameters.dart
@@ -1,12 +1,38 @@
-void main() {
-  print('Optional Positional Parameters in Dart');
-  // Calling the Function
-  testParameter(1);
-  // Calling the Function with Optional Positional Parameter
-  testParameter(2, 3);
-}
+// Dart - Functions: Parameters
+
+// Optional Positional Parameters
+
+// ignore_for_file: unnecessary_null_comparison
 
 // Function Declaration with Optional Positional Parameter
 testParameter(p1, [o1]) {
   print('Parameter: $p1 Optional Positional Parameter: $o1');
+}
+
+// Another Example
+String myMessage(String who, [String? what, String? where]) {
+  var message = '$who';
+  if (what != null && where == null) {
+    message = '$message said $what';
+  } else if (where != null) {
+    message = '$message said $what at $where';
+  }
+  return message;
+}
+
+void main() {
+  print('Optional Positional Parameters in Dart');
+
+  print(''); //  Line-Break
+
+  // Calling the Function
+  testParameter(1);
+  // Calling the Function with Optional Positional Parameter
+  testParameter(2, 3);
+
+  print(''); //  Line-Break
+
+  // Another Example
+  var result = myMessage('Ahmed', 'How are you?');
+  print(result);
 }
