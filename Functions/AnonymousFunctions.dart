5af8f27387d1caeddeff231cc1fea1d57bf68db1
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-02-24 09:07:54 +0500

    Added more examples & removed some commits

--- Functions/AnonymousFunctions.dart
+++ Functions/AnonymousFunctions.dart
@@ -1,8 +1,6 @@
-// Anonymous Functions in Dart
+// Dart - Functions : Anonymous Functions
 
-// ignore: todo
-// TODO: Figure Out Difference between lambda, anonymous, fat-arrow => functions
-// ?? What's the difference?
+// Anonymous functions are sometimes known as lambda functions or closures.
 
 // Normal Function
 void addTwoNumbers(int a, int b) {
@@ -20,20 +18,30 @@ void main() {
   // Anonymous Function Example: 2
   Function addNumbers = (int x, int y) => print(x + y);
 
-  // Calling anonymous function 1
+  // Calling Anonymous Function 1
   print('Anonymous Function');
   addTwoNumbers(5, 7);
-  // Calling anonymous function 2
+
+  // Calling Anonymous Function 2
   addNumbers(4, 6);
 
-  // Calling normal function
+  print(''); //  Line-Break
+
+  // Calling Normal Function
   print('Normal Function');
   addTwoNumbers(5, 7);
 
-  // Another Anonymous Function
+  print(''); //  Line-Break
+
+  // Another Example of Anonymous Function
   var list = ["John", "Doe", "Smith", "Alex"];
   print("Dart Anonymous Function");
   list.forEach((item) {
     print('${list.indexOf(item)}: $item');
   });
+
+  print(''); //  Line-Break
+
+  var aList = [1, 2, 3];
+  aList.forEach((item) => print(item * item * item));
 }
