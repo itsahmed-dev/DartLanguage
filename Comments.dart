8e299407628bd1f4b0b7bca3f5efc178b4533ce8
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-02-25 08:49:33 +0500

    Updated 'Document Comment' discription

--- Comments.dart
+++ Comments.dart
@@ -11,18 +11,20 @@
 // This is a Single Line Comment
 
 /*
-  Multiline comments:
-  It's a Multi-Line Comment.
+Multiline comments:
+It's a Multi-Line Comment.
 */
 
 /**Document Comments 1:
- * [main] Function 
+ *[main] Function is entry point of Dart Program.
+ *This is another way of writing document comments
  */
 
 /// Document Comments 2:
-/// [main] is a function and this is Document comment
-/// Notice that main function in brackets above corresponds to main function below
-/// That's what makes document comments different.
+/// Is used for documentation purposes.
+/// Click [main] here to see highlighting it's position in the program.
+/// or hover over [main] in the program and this comment will be shown.
+/// That's what document comments do.
 
 void main() {
   print('بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ');
