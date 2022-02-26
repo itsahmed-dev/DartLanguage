82d636673b47554ee6cb36564cb70f1988387c3c
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-02-26 15:36:52 +0500

    Game exercise by Andrea

---
+++ RockPaperScissor.dart
@@ -0,0 +1,43 @@
+// Rock-Paper-Scissor Game Exercise by Andrea
+
+import 'dart:io';
+
+import 'dart:math';
+
+enum Move { rock, paper, scissor }
+
+void main() {
+  final rng = Random();
+  while (true) {
+    stdout.write('Rock, paper or scissor? (r/p/s) ');
+    final input = stdin.readLineSync();
+    if (input == 'r' || input == 'p' || input == 's') {
+      var playerMove;
+      if (input == 'r') {
+        playerMove = Move.rock;
+      } else if (input == 'p') {
+        playerMove = Move.paper;
+      } else {
+        playerMove = Move.scissor;
+      }
+      final random = rng.nextInt(3);
+      final aiMove = Move.values[random];
+      print('Player Move: $playerMove');
+      print('AI Move: $aiMove');
+      if (playerMove == aiMove) {
+        print('It\'s a Draw');
+      } else if (playerMove == Move.rock && aiMove == Move.scissor ||
+          playerMove == Move.paper && aiMove == Move.rock ||
+          playerMove == Move.scissor && aiMove == Move.paper) {
+        print('You Win');
+      } else {
+        print('You Lose');
+      }
+    } else if (input == 'q') {
+      print('Game Over');
+      break;
+    } else {
+      print('Invalid Input');
+    }
+  }
+}
