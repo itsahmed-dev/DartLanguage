63e31af29fe43a34ef286dafc536b39995219317
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-02-27 11:45:41 +0500

    Exercises : Sum of the items in a list

---
+++ Andrea/SumListItems.dart
@@ -0,0 +1,10 @@
+// Andrea Exercises : Sum of the items in a list
+
+void main() {
+  List list = [1, 3, 5, 7, 9];
+  int sum = 0;
+  for (int item in list) {
+    sum += item;
+  }
+  print('The sum of $list: $sum');
+}
