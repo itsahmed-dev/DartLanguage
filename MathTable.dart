a13d681ad856b46baa6bc1b91a50b345a70aa707
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-03-12 15:00:16 +0500

    Added do-while loop

--- MathTable.dart
+++ MathTable.dart
@@ -5,17 +5,22 @@
 import 'dart:io';
 
 void main() {
-  int tab, from, to;
-  stdout.write('Table Of? ');
-  tab = int.parse(stdin.readLineSync()!);
+  String run;
+  do {
+    int tab, from, to;
+    stdout.write('Table Of? ');
+    tab = int.parse(stdin.readLineSync()!);
 
-  stdout.write('Table From? ');
-  from = int.parse(stdin.readLineSync()!);
+    stdout.write('Table From? ');
+    from = int.parse(stdin.readLineSync()!);
 
-  stdout.write('Table To? ');
-  to = int.parse(stdin.readLineSync()!);
+    stdout.write('Table To? ');
+    to = int.parse(stdin.readLineSync()!);
 
-  for (var i = from; i <= to; i++) {
-    print('$tab X $i = ${tab * i}');
-  }
+    for (var i = from; i <= to; i++) {
+      print('$tab X $i = ${tab * i}');
+    }
+    stdout.write('Run Again? (Y/N) ');
+    run = stdin.readLineSync()!;
+  } while (run == 'Y' || run == 'y');
 }
