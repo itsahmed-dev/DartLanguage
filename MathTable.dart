ab5190c4d91be307823d58c5f839ccb8b5b7de8f
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-03-12 10:17:49 +0500

    Math Table Exercise

---
+++ MathTable.dart
@@ -0,0 +1,21 @@
+// Dart - Exercises
+
+// Math Table
+
+import 'dart:io';
+
+void main() {
+  int tab, from, to;
+  stdout.write('Table Of? ');
+  tab = int.parse(stdin.readLineSync()!);
+
+  stdout.write('Table From? ');
+  from = int.parse(stdin.readLineSync()!);
+
+  stdout.write('Table To? ');
+  to = int.parse(stdin.readLineSync()!);
+
+  for (var i = from; i <= to; i++) {
+    print('$tab X $i = ${tab * i}');
+  }
+}
