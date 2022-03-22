d70c4d3d35f8041109fdafec4b8ed3347ddaf2a3
Muhammad Ahmed <itsahmed.dev@gmail.com>
2022-03-22 09:09:17 +0500

    Extensions (Flutterly)

---
+++ ObjectOriented/Extensions2.dart
@@ -0,0 +1,12 @@
+// Dart - Object Oriented Programming:
+
+// Extensions (Flutterly)
+
+extension IntegerExtension on int {
+  int get answerToLife => 42;
+}
+
+void main() {
+  1.answerToLife;
+  print('1.answerToLife --> ${1.answerToLife}');
+}
